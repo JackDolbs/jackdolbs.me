66e1e90e17212ac0f85b4b715eb7275f4c60bbd9
Jack Dolby <92309491+JackDolbs@users.noreply.github.com>
2024-02-19 18:40:16 +0000

    edits to site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,13 @@
 module.exports = {
-  title: 'Jack Dolbs',
-  description: 'I like to build things.',
-  dest: 'public',
-  plugins: [
-    '@vuepress/last-updated',
-    ['@vuepress/google-analytics', { ga: 'UA-93673521-3' }],
-    ['@vuepress/search', { searchHotkeys: [] }],
-  ],
   head: [
-  [
-  'link',
- {
-rel: 'stylesheet',
-href: 'https://gitcdn.xyz/repo/kiwicopple/assets/master/public/css/circular.css',
-},
-],
-],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://gitcdn.xyz/repo/kiwicopple/assets/master/public/css/circular.css',
+      },
+    ],
+  ],
   themeConfig: {
     nav: [
       { text: 'Blog', link: '/blog/' },
@@ -27,23 +19,16 @@ href: 'https://gitcdn.xyz/repo/kiwicopple/assets/master/public/css/circular.css'
       '/blog/': [
         'hello-world',
       ],
-      '/gists/': ['vscode-snippets', 'ecto-phoenix', 'kill-processes'],
-      // '/levels/': ['business', 'life', 'leadership', 'learning', 'tech'],
       '/knowledge/': [
-        // {
-        //   title: 'Business Practices',
-        //   collapsable: false,
-        //   children: ['emyth', 'toyota-way'],
-        // },
         {
           title: 'Miscellaneous',
           collapsable: false,
           children: [
             // list of name of md files here, formatted like this:
             //'chess',
-           // 'climbing',
+            //'climbing',
             //'consciousness',
-           // 'languages',
+            //'languages',
             //'zoology',
           ],
         },
@@ -53,9 +38,9 @@ href: 'https://gitcdn.xyz/repo/kiwicopple/assets/master/public/css/circular.css'
           children: [
             // list of name of md files here, formatted like this:
             //'chess',
-           // 'climbing',
+            //'climbing',
             //'consciousness',
-           // 'languages',
+            //'languages',
             //'zoology',
           ],
         },
@@ -65,28 +50,12 @@ href: 'https://gitcdn.xyz/repo/kiwicopple/assets/master/public/css/circular.css'
           children: [
             // list of name of md files here, formatted like this:
             //'chess',
-           // 'climbing',
-            //'consciousness',
-           // 'languages',
-            //'zoology',
-          ],
-        },
-        {
-          title: 'People Of Interest',
-          collapsable: false,
-          children: [
-            // list of name of md files here, formatted like this:
-            //'chess',
-           // 'climbing',
-            //'consciousness',
-           // 'languages',
-            //'zoology',
           ],
         },
       ],
     },
-    docsRepo: 'https://github.com/jackdolbs/jackdolbs.me/',
+    docsRepo: 'https://github.com/JackDolbs/jackdolbs.me/tree/main/docs',
     docsDir: 'docs',
     editLinks: true,
   },
-}
+};
